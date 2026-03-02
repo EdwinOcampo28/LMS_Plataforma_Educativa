90a336618464ac4afbc9bb51419021933c102f2c
EdwinOcampo28 <235344181+EdwinOcampo28@users.noreply.github.com>
2026-03-02 08:42:16 -0500

    Docentes.js

---
+++ Proyecto_LMS_Edwin_Nicolas/js/docentes.js
@@ -0,0 +1,72 @@
+
+let docentes=JSON.parse(localStorage.getItem("docentes"))||[]
+
+const tabla=document.getElementById("tablaDocentes")
+const buscar=document.getElementById("buscar")
+
+render()
+
+docenteForm.addEventListener("submit",e=>{
+
+e.preventDefault()
+
+docentes.push({
+
+codigo:codigo.value,
+nombre:nombres.value+" "+apellidos.value,
+email:email.value,
+area:area.value
+
+})
+
+guardar()
+
+})
+
+buscar.addEventListener("input",()=>{
+
+render(buscar.value)
+
+})
+
+function guardar(){
+
+localStorage.setItem("docentes",JSON.stringify(docentes))
+
+render()
+
+}
+
+function eliminar(i){
+
+docentes.splice(i,1)
+
+guardar()
+
+}
+
+function render(filtro=""){
+
+tabla.innerHTML=""
+
+docentes
+.filter(d=>d.nombre.toLowerCase().includes(filtro.toLowerCase()))
+.forEach((d,i)=>{
+
+tabla.innerHTML+=`
+
+<tr>
+<td>${d.codigo}</td>
+<td>${d.nombre}</td>
+<td>${d.email}</td>
+<td>${d.area}</td>
+<td>
+<button onclick="eliminar(${i})">Eliminar</button>
+</td>
+</tr>
+
+`
+
+})
+
+}
