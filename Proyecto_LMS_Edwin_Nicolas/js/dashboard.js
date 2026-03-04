27993117b28f88abaf8e8e070f04a8855452d4b2
EdwinOcampo28 <235344181+EdwinOcampo28@users.noreply.github.com>
2026-03-04 15:51:41 -0500

    Dashboard.Js

--- Proyecto_LMS_Edwin_Nicolas/js/dashboard.js
+++ Proyecto_LMS_Edwin_Nicolas/js/dashboard.js
@@ -1,8 +1,14 @@
 
-let cursos=JSON.parse(localStorage.getItem("cursos"))||[]
-let docentes=JSON.parse(localStorage.getItem("docentes"))||[]
-let admins=JSON.parse(localStorage.getItem("administrativos"))||[]
+let cursos = JSON.parse(localStorage.getItem("cursos")) || []
+let docentes = JSON.parse(localStorage.getItem("docentes")) || []
+let admins = JSON.parse(localStorage.getItem("administrativos")) || []
 
-document.getElementById("totalCursos").textContent=cursos.length
-document.getElementById("totalDocentes").textContent=docentes.length
-document.getElementById("totalAdmins").textContent=admins.length
+document.getElementById("totalCursos").textContent = cursos.length
+document.getElementById("totalDocentes").textContent = docentes.length
+document.getElementById("totalAdmins").textContent = admins.length
+
+const usuario = sessionStorage.getItem("usuarioActivo")
+
+if (!usuario) {
+    window.location = "index.html"
+}
