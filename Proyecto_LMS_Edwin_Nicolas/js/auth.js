c5d714271c11926aea654d0a5ab465cf2a40114d
EdwinOcampo28 <235344181+EdwinOcampo28@users.noreply.github.com>
2026-03-04 15:51:09 -0500

    Auth.Js

--- Proyecto_LMS_Edwin_Nicolas/js/auth.js
+++ Proyecto_LMS_Edwin_Nicolas/js/auth.js
@@ -1,22 +1,19 @@
+const loginForm = document.getElementById("loginForm")
 
-document.getElementById("loginForm").addEventListener("submit",e=>{
-e.preventDefault()
+loginForm.addEventListener("submit", function (e) {
+    e.preventDefault()
 
-let email=document.getElementById("email").value
-let password=document.getElementById("password").value
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
 
-let admins=JSON.parse(localStorage.getItem("administrativos"))||[]
+    let admins = JSON.parse(localStorage.getItem("administrativos")) || []
 
-let admin=admins.find(a=>a.email===email && a.password===password)
+    const user = admins.find(a => a.email === email && a.password === password)
 
-if(admin){
-
-window.location="dashboard.html"
-
-}else{
-
-alert("Credenciales incorrectas")
-
-}
-
+    if (user) {
+        sessionStorage.setItem("usuarioActivo", JSON.stringify(user))
+        window.location = "dashboard.html"
+    } else {
+        alert("Credenciales incorrectas")
+    }
 })
