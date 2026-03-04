b8bdb3ee0053c31fa43a29684a0dbe7b0ab82bac
EdwinOcampo28 <235344181+EdwinOcampo28@users.noreply.github.com>
2026-03-04 08:55:56 -0500

    Docentes.Js

--- Proyecto_LMS_Edwin_Nicolas/js/docentes.js
+++ Proyecto_LMS_Edwin_Nicolas/js/docentes.js
@@ -1,72 +1,64 @@
+let docentes = JSON.parse(localStorage.getItem("docentes")) || []
 
-let docentes=JSON.parse(localStorage.getItem("docentes"))||[]
+const tabla = document.getElementById("tablaDocentes")
+const buscar = document.getElementById("buscar")
+const docenteForm = document.getElementById("docenteForm")
 
-const tabla=document.getElementById("tablaDocentes")
-const buscar=document.getElementById("buscar")
-
-render()
-
-docenteForm.addEventListener("submit",e=>{
-
-e.preventDefault()
-
-docentes.push({
-
-codigo:codigo.value,
-nombre:nombres.value+" "+apellidos.value,
-email:email.value,
-area:area.value
-
-})
-
-guardar()
-
-})
-
-buscar.addEventListener("input",()=>{
-
-render(buscar.value)
-
-})
-
-function guardar(){
-
-localStorage.setItem("docentes",JSON.stringify(docentes))
+const codigo = document.getElementById("codigo")
+const nombres = document.getElementById("nombres")
+const apellidos = document.getElementById("apellidos")
+const email = document.getElementById("email")
+const area = document.getElementById("area")
 
 render()
 
-}
+docenteForm.addEventListener("submit", e => {
 
-function eliminar(i){
+    e.preventDefault()
 
-docentes.splice(i,1)
+    docentes.push({
+        codigo: codigo.value,
+        nombre: nombres.value + " " + apellidos.value,
+        email: email.value,
+        area: area.value
+    })
 
-guardar()
-
-}
-
-function render(filtro=""){
-
-tabla.innerHTML=""
-
-docentes
-.filter(d=>d.nombre.toLowerCase().includes(filtro.toLowerCase()))
-.forEach((d,i)=>{
-
-tabla.innerHTML+=`
-
-<tr>
-<td>${d.codigo}</td>
-<td>${d.nombre}</td>
-<td>${d.email}</td>
-<td>${d.area}</td>
-<td>
-<button onclick="eliminar(${i})">Eliminar</button>
-</td>
-</tr>
-
-`
-
+    guardar()
+    docenteForm.reset()
 })
 
+buscar.addEventListener("input", () => {
+    render(buscar.value)
+})
+
+function guardar() {
+    localStorage.setItem("docentes", JSON.stringify(docentes))
+    render()
+}
+
+function eliminar(i) {
+    docentes.splice(i, 1)
+    guardar()
+}
+
+function render(filtro = "") {
+
+    tabla.innerHTML = ""
+
+    docentes
+        .filter(d => d.nombre.toLowerCase().includes(filtro.toLowerCase()))
+        .forEach((d, i) => {
+
+            tabla.innerHTML += `
+            <tr>
+                <td>${d.codigo}</td>
+                <td>${d.nombre}</td>
+                <td>${d.email}</td>
+                <td>${d.area}</td>
+                <td>
+                    <button onclick="eliminar(${i})">Eliminar</button>
+                </td>
+            </tr>
+            `
+        })
 }
