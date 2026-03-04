d1c04080f40c15fdf5a32b03bb24c2608d53abd3
EdwinOcampo28 <235344181+EdwinOcampo28@users.noreply.github.com>
2026-03-04 15:51:18 -0500

    Cursos.Js

--- Proyecto_LMS_Edwin_Nicolas/js/cursos.js
+++ Proyecto_LMS_Edwin_Nicolas/js/cursos.js
@@ -1,93 +1,93 @@
 
-let cursos=JSON.parse(localStorage.getItem("cursos"))||[]
+let cursos = JSON.parse(localStorage.getItem("cursos")) || []
 
-const tabla=document.getElementById("tablaCursos")
-
-render()
-
-cursoForm.addEventListener("submit",e=>{
-
-e.preventDefault()
-
-cursos.push({
-
-codigo:codigo.value,
-nombre:nombre.value,
-descripcion:descripcion.value,
-docente:docente.value,
-modulos:[]
-
-})
-
-guardar()
-
-})
-
-moduloForm.addEventListener("submit",e=>{
-
-e.preventDefault()
-
-let curso=cursos.find(c=>c.codigo===cursoModulo.value)
-
-if(!curso) return alert("Curso no encontrado")
-
-curso.modulos.push({
-
-nombre:moduloNombre.value,
-lecciones:[]
-
-})
-
-guardar()
-
-})
-
-leccionForm.addEventListener("submit",e=>{
-
-e.preventDefault()
-
-let curso=cursos.find(c=>c.codigo===cursoLeccion.value)
-
-if(!curso) return
-
-let modulo=curso.modulos.find(m=>m.nombre===moduloLeccion.value)
-
-if(!modulo) return
-
-modulo.lecciones.push({
-
-titulo:titulo.value,
-contenido:contenido.value
-
-})
-
-guardar()
-
-})
-
-function eliminar(i){
-
-cursos.splice(i,1)
-
-guardar()
-
-}
-
-function guardar(){
-
-localStorage.setItem("cursos",JSON.stringify(cursos))
+const tabla = document.getElementById("tablaCursos")
 
 render()
 
+cursoForm.addEventListener("submit", e => {
+
+    e.preventDefault()
+
+    cursos.push({
+
+        codigo: codigo.value,
+        nombre: nombre.value,
+        descripcion: descripcion.value,
+        docente: docente.value,
+        modulos: []
+
+    })
+
+    guardar()
+
+})
+
+moduloForm.addEventListener("submit", e => {
+
+    e.preventDefault()
+
+    let curso = cursos.find(c => c.codigo === cursoModulo.value)
+
+    if (!curso) return alert("Curso no encontrado")
+
+    curso.modulos.push({
+
+        nombre: moduloNombre.value,
+        lecciones: []
+
+    })
+
+    guardar()
+
+})
+
+leccionForm.addEventListener("submit", e => {
+
+    e.preventDefault()
+
+    let curso = cursos.find(c => c.codigo === cursoLeccion.value)
+
+    if (!curso) return
+
+    let modulo = curso.modulos.find(m => m.nombre === moduloLeccion.value)
+
+    if (!modulo) return
+
+    modulo.lecciones.push({
+
+        titulo: titulo.value,
+        contenido: contenido.value
+
+    })
+
+    guardar()
+
+})
+
+function eliminar(i) {
+
+    cursos.splice(i, 1)
+
+    guardar()
+
 }
 
-function render(){
+function guardar() {
 
-tabla.innerHTML=""
+    localStorage.setItem("cursos", JSON.stringify(cursos))
 
-cursos.forEach((c,i)=>{
+    render()
 
-tabla.innerHTML+=`
+}
+
+function render() {
+
+    tabla.innerHTML = ""
+
+    cursos.forEach((c, i) => {
+
+        tabla.innerHTML += `
 
 <tr>
 <td>${c.codigo}</td>
@@ -100,6 +100,6 @@ tabla.innerHTML+=`
 
 `
 
-})
+    })
 
 }
